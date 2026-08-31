b8e7f3f3c58e0bf8bade42687d79a710e21b6034
sdesrocher2 <87208196+sdesrocher2@users.noreply.github.com>
2026-08-31 13:55:31 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,11 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("Screen-to-Dispatch installed.");
+chrome.action.onClicked.addListener(async (tab) => {
+  await chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: () => {
+      window.postMessage({
+        source: "screen-to-dispatch",
+        type: "START_SELECTION"
+      }, "*");
+    }
+  });
 });
