ee425d13fbe36b874595d7612be5e9972e161dfa
sdesrocher2 <87208196+sdesrocher2@users.noreply.github.com>
2026-08-31 13:46:12 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,16 +1,1 @@
-let selectionMode = false;
-
-document.addEventListener("mouseover", handleMouseOver);
-document.addEventListener("mouseout", handleMouseOut);
-
-function handleMouseOver(event) {
-  if (!selectionMode) return;
-
-  event.target.style.outline = "2px solid blue";
-}
-
-function handleMouseOut(event) {
-  if (!selectionMode) return;
-
-  event.target.style.outline = "";
-}
+console.log("Screen-to-Dispatch content script loaded.");
